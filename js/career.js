c3cf31bd7d7a9cb6a2915a4e8b6f6ecbb7dd281d
Rohan deep Kujur <rdkujur1@gmail.com>
2023-06-03 09:43:45 +0530

    some changes and career page added

---
+++ js/career.js
@@ -0,0 +1,22 @@
+const profiles = document.querySelectorAll(".intern-profile");
+
+const numberOfProfile = profiles.length;
+var profileNumber = 0;
+
+var playSlider;
+var repeater = () => {
+  playSlider = setInterval(function () {
+    profiles.forEach((profile) => {
+      profile.classList.remove("active");
+    });
+    profileNumber++;
+
+    if (profileNumber > numberOfProfile - 1) {
+      profileNumber = 0;
+    }
+    profiles[profileNumber].classList.add("active");
+  }, 5000); // 2000 is equal to 2 section in we want to increase or decrease the delay than we have to just increase or decrease seconds
+};
+repeater();
+
+
